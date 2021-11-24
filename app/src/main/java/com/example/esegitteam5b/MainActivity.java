e20168ab7dda568d6f43b339bec5adc5c2ec5c02
APalazzetti <alexpalazzetti@gmail.com>
2021-11-24 12:44:25 +0100

    Creazione bottone con toast

--- app/src/main/java/com/example/esegitteam5b/MainActivity.java
+++ app/src/main/java/com/example/esegitteam5b/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.esegitteam5b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
+    Button invio;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        invio= (Button)findViewById(R.id.b1);
+        context=getApplicationContext();
+    }
+    public void message(View v){
+        int duration = Toast.LENGTH_LONG;
+        Toast  toast = Toast.makeText(context,R.string.text,duration);
+        toast.show();
     }
 }
